f9ff2afae8724443632bf7fbaad799825afece65
Danudi <danusilva1997@gmail.com>
2021-12-08 17:31:58 +0530

    add category crud

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -28,3 +28,55 @@ const addCatView = (req, res, next) => {
         err: false
     })
 }
+
+const addCategory = (req, res, next) => {
+    console.log("addCategory1")
+    var sqlAll = `SELECT * FROM category WHERE name='${req.body.name}'`;
+    mysqlConnection.query(sqlAll, function (err1, result) {
+        console.log("addCategory2")
+        console.log(err1)
+        console.log(result)
+        if (result.length >= 1) {
+            console.log("addCategory3")
+            res.render('addCategory', {
+                title: "Add Category",
+                err: true,
+                msg: req.body.name + " alredy exist. Please try new category"
+            })
+        } else {
+            console.log("addCategory4")
+            var sql = `INSERT INTO category(name,image,status,createAt) VALUES
+    ('${req.body.name}','${req.files[0].originalname}','${false}',NOW())`;
+
+            mysqlConnection.query(sql, function (err, result) {
+                console.log("addCategory4")
+                console.log(result)
+                console.log(err)
+                if (err) {
+                    res.render('addCategory', {
+                        title: "Add Category",
+                        err: true,
+                        msg: "Please try again"
+                    })
+                } else {
+                    res.render('addCategory', {
+                        title: "Add Category",
+                        err: false,
+                        msg: "Added category sucess"
+                    })
+                }
+            })
+        }
+    })
+}
+
+const delete_category = (req, res, next) => {
+    console.log("delete_category1")
+    const cat_id = req.params.cat_id;
+    var sql = `DELETE FROM category WHERE cat_id='${cat_id}'`;
+    mysqlConnection.query(sql, function (err, result1) {
+        if(err) throw err;
+        req.flash('notify', 'Redirect successful!')
+        res.redirect('/admin/categories');
+    })
+}
