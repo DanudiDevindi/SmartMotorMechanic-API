462cb06a71c4fc8d839a25653e3c9caf278bce83
Danudi <danusilva1997@gmail.com>
2021-12-10 14:31:49 +0530

    service routes

---
+++ routes/service_route.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const checkAuth=require('../middleware/check_auth');
+const multer=require('multer');
+const {allserviceView,serviceTypesView,deleteService,blockService,approveService,addServiceView,addServiceType,addService,addServiceTypeView,deleteServiceType,serviceView,viewServiceType,editServiceType}=require("../controllers/admin/serviceController");
+const {createService,editService,userServices,deleteUserService,allServices,allServicesWithPagination,allServicesWithFilter,allService_types,createServiceRate,getRateSummery,getServiceRate}=require("../controllers/serviceController");
+const router = express.Router();
+
+const storage=multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    } ,
+    filename: function (req,file,cb) {
+        cb(null, file.originalname)
+    }
+});
+const upload=multer({
+    storage: storage,
+    limit:{
+        filesize: 1024*1024*5
+    }
+});
