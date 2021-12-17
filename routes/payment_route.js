58a2a20b665c437e9650d4c7daf7e28e362f9cd9
Danudi <danusilva1997@gmail.com>
2021-12-17 16:55:26 +0530

    payment routes

---
+++ routes/payment_route.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const multer=require('multer');
+const {paymnetDurationView,addPaymnetDurationView,addPaymnetDuration,deletePaymnetDuration,viewPaymentDuration,editPaymentDuration}=require("../controllers/admin/paymnetController");
+const {getPaymnetDuration,createUserPayment,getPayPal,testing}=require("../controllers/paymentController");
+const router = express.Router();
+const checkAuth=require('../middleware/check_auth');
+
+
+const storage=multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    } ,
+    filename: function (req,file,cb) {
+        cb(null, file.originalname)
+    }
+});
+const upload=multer({
+    storage: storage,
+    limit:{
+        filesize: 1024*1024*5
+    }
+});
